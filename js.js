0d61b363afcfdfff2e3e9fa58c9c8df3a0f024a2
Evgen Sikora <arnika200@gmail.com>
2022-01-11 15:20:58 +0200

    t5 done

--- js.js
+++ js.js
@@ -54,8 +54,8 @@ function processArray(arr, factorial){
 }
 
 function factorial (n){
-    let result = 1;
-    for (let i = 0; i < n; i++){
+    let result = 1;                //factorial(0) === 1 https://uk.wikipedia.org/wiki/%D0%A4%D0%B0%D0%BA%D1%82%D0%BE%D1%80%D1%96%D0%B0%D0%BB
+    for (let i = 0; i < n; i++) {
         result = result * (n - i);
     }
     return result;
@@ -65,3 +65,53 @@ console.log(processArray([1, 2, 3, 4, 5], factorial));
 arrExmpl=[1, 2, 3, 4, 5];
 processArray(arrExmpl, factorial);
 console.log(arrExmpl); // the source array arr does not change, because map() create new array
+
+//5) Write a checkAdult(age) function whose input parameter is the age of the user age. The function checks whether the set age parameter is set correctly,
+// if it is set incorrectly, the corresponding error should be generated and displayed in the console:
+// - if the age value has not been set, you need to create the following error: "Please, enter your age",
+// - If you set a negative age value, you need to create the following error: "Please, enter positive number",
+// - if a non-numeric value of age was specified, you need to create the following error: "Please, enter number",
+// - if the integer value of age was not specified, you need to create the following error: "Please, enter Integer number",
+// - If the user is under 18, you need to create the following error: "Access denied - you are too young!".
+// If there is no error, the message “Access allowed” is displayed in the console.
+// In the function, implement the handling of possible exceptions, providing the output to the console of the name and description of the error.
+// Regardless of whether the age parameter was set correctly or incorrectly, the message “Age verification complete” should be displayed at the end of the test.
+//Function usage example:
+// checkAdult(15);    //Error Access denied - you are too young!
+//                   //Age verification complete
+// checkAdult(25);  //Access allowed
+//                  //Age verification complete
+
+function checkAdult(age){
+    try {
+        if(age > 18) console.log('Access allowed');
+        if(age === '') {
+            throw new Error("Please, enter your age");
+        }
+        if(age < 0){
+            throw  new Error("Please, enter positive number");
+        }
+        if(typeof age!=="number"){
+            throw  new Error("Please, enter number");
+        }
+        if (!Number.isInteger(age)){
+            throw new Error("Please, enter Integer number");
+        }
+        if (age < 18){
+            throw new Error("Access denied - you are too young!")
+        }
+    }
+    catch (e) {
+        console.log(e.name);
+        console.log(e.message);
+    }
+    finally {
+        console.log('Age verification complete');
+    }
+}
+checkAdult(23);
+checkAdult(15);
+checkAdult();
+checkAdult('fff');
+checkAdult(-4);
+checkAdult(2.5);

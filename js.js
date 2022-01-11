60258a8d8e22117a44fd6d4877965babcb2ccc17
Evgen Sikora <arnika200@gmail.com>
2022-01-11 12:37:07 +0200

    t3 done

--- js.js
+++ js.js
@@ -22,3 +22,18 @@ function combineArray(arr1, arr2) {
     return arr3;
 }
 console.log(combineArray([12, "User01", 22, true, -8], ["Index", 6, null, 15]));
+//3)Implement the longestLogin(loginList) function, which takes an array of user logins loginList  and returns the longest login.
+// If the logins of the same length are the longest in the array, the login element with the largest index is returned.
+// Tip: You can use the reduce() method to solve the task.
+// Function examples:
+// longestLogin(["serg22", "tester_2", "Prokopenko", "guest"]);   //  Prokopenko
+// longestLogin(["user1", "user2", "333", "user4", "aa"]);   //  user4
+function longestLogin(loginList) {
+    let longest = loginList.reduce(function (a, b) {
+        if (a.length > b.length) return a;
+        if (a.length <= b.length) return b;
+    });
+    return longest;
+}
+console.log(longestLogin(["serg22", "tester_2", "Prokopenko", "guest"]));
+console.log(longestLogin(["user1", "user2", "333", "user4", "aaaaa"]));

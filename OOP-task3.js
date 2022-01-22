58d86a3cd7e66893a6c28df0a31392fc5f71e153
evgen sikora <arnika200@gmail.com>
2022-01-22 15:44:56 +0200

    t3 oop done

--- OOP-task3.js
+++ OOP-task3.js
@@ -18,7 +18,6 @@ class Worker {
         console.log(this.fullName + ' salary: ' + this.salary);
     }
 
-    exp = 1.2;
 
     showSalaryWithExperience() {
         this.salary = this.dayRate * this.workingDays * this.exp;
@@ -32,21 +31,33 @@ class Worker {
     set setExp(value) {
         this.exp = value;
     }
-
-/*function sort(){
-    salary.sort(function(a,b) {
-        return a.born - b.born;
-    });
-    console.log('by date:');
-    console.log(byDate);*/
 }
 const worker1 = new Worker("John Johnson", 20, 23);
 console.log(worker1.fullName);
 worker1.showSalary();
+worker1.setExp = 1.2;
 console.log("New experience: " + worker1.showExp);
 worker1.showSalaryWithExperience();
 worker1.setExp = 1.5;
 console.log("New experience: " + worker1.showExp);
 worker1.showSalaryWithExperience();
+
 const worker2 = new Worker("Tom Tomson", 48, 22);
+console.log(worker2.fullName);
+worker2.showSalary();
+
 const worker3 = new Worker("Andy Ander", 29, 23);
+console.log(worker3.fullName);
+worker3.showSalary();
+
+let employees = [];
+employees.push(worker1,worker2,worker3)
+employees.sort(function(a ,b) {
+    return a.salary - b.salary
+})
+
+let result = []
+for (let i = 0; i<employees.length; i++) {
+    result[i] = employees[i].fullName + ': ' + employees[i].salary
+}
+console.log('Sorted salary:' +'\n'+ result[0]+'\n' + result[1] +'\n' + result[2])
